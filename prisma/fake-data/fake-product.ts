20d95955c3c584660087a4f1e02c628c89eefecf
haloapping <alfiyanto.kondolele@gmail.com>
2025-03-23 21:31:30 +0700

    fix: seed

---
+++ prisma/fake-data/fake-product.ts
@@ -0,0 +1,114 @@
+import { Product } from "../../src/types/product";
+
+export const fakeProducts: Product[] = [
+  {
+    name: "Udang Vaname Utuh",
+    slug: "udang-vaname-utuh",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/udang-vaname-utuh.webp",
+    price: BigInt(64900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Udang vannamei lokal beku. Masih ada kepala dan kulit. Cocok untuk ditumis, masak saus padang, saus tiram, saus asam manis, dimasak nasi goreng, dan lain sebagainya.",
+  },
+  {
+    name: "Udang Vaname Kupas",
+    slug: "udang-vaname-kupas",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/udang-vaname-kupas.webp",
+    price: BigInt(69900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Sudah kupas kulit, dan buang ekor serta kepala. Lebih praktis dan mudah diolah. Udang  ini direndam dengan air panas untuk melepaskan cangkang dan kepalanya, tetapi kondisinya masih mentah sehingga perlu diolah kembali untuk proses pematangannya. Cocok untuk menu MPASI.",
+  },
+  {
+    name: "Kerang Tiram Kupas",
+    slug: "kerang-tiram-kupas",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/kerang-tiram-kupas.webp",
+    price: BigInt(69900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Saran penyajian :  tumis tiram, tiram asam manis, tiram saos padang, tiram pedas, tiram kuah bumbu kuning, dll. Sudah di Kupas, tidak ada cangkang.",
+  },
+  {
+    name: "Cumi Tube Kupas Golden Seafood",
+    slug: "cumi-tube-kupas-golden-seafood",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/cumi-tube-kupas-golden-seafood.webp",
+    price: BigInt(49900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Cumi tube lokal beku atau cumi kupas fillet. Sudah bersih. Tanpa kepala dan isi perut. Berat produk dapat berkurang 10% dari berat beku",
+  },
+  {
+    name: "Ikan Tongkol Abu Steak",
+    slug: "ikan-tongkol-abu-steak",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/ikan-tongkol-abu-steak.webp",
+    price: BigInt(39900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Potongan ikan tongkol beku berbentuk steak. Cocok dimasak balado, bumbu kuning, cabai hijau, dan lain-lain. Berat produk dapat berkurang 10% dari berat beku.",
+  },
+  {
+    name: "Ikan Mujair Utuh",
+    slug: "ikan-mujair-utuh",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/ikan-mujair-utuh.webp",
+    price: BigInt(40300),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Ikan mujair lokal beku utuh. Sisik dan isi perut sudah dibersihkan. Cocok dimasak gulai, bumbu kuning, pesmol, ikan bakar, ikan goreng, dan lain-lain. Berat produk dapat berkurang 10% dari berat beku.",
+  },
+  {
+    name: "Ikan Kembung Banjar",
+    slug: "ikan-kembung-banjar",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/ikan-kembung-banjar.webp",
+    price: BigInt(40300),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Ikan kembung utuh. Masih ada sisik, insang, dan isi perut. Dagingnya gurih dan padat. Cocok untuk menu MPASI.",
+  },
+  {
+    name: "Ikan Salmon Fillet",
+    slug: "ikan-salmon-fillet",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/ikan-salmon-fillet.webp",
+    price: BigInt(69900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Salmon beku yang sudah dipotong fillet. Bebas duri.  Cocok untuk steak, salmon kuah asam, dan lain sebagainya. Dapat ditambahkan dalam menu MPASI anak. Berat produk dapat berkurang 10% dari berat bekunya. Produk ini dapat digunakan sebagai menu MPASI.",
+  },
+  {
+    name: "Ikan Tuna Giling",
+    slug: "ikan-tuna-giling",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/ikan-tuna-giling.webp",
+    price: BigInt(49900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Ikan tuna giling yang diproses tanpa bahan pengawet. Lebih mudah diolah menjadi berbagai kreasi masakan.",
+  },
+  {
+    name: "Ikan Lele Utuh",
+    slug: "ikan-lele-utuh",
+    imageUrl: "https://ik.imagekit.io/xc0cxidhx/pasar-ikang/ikan-lele-utuh.webp",
+    price: BigInt(29900),
+    unit: "gr",
+    stock: BigInt(5000000),
+    sold: BigInt(100),
+    description:
+      "Ikan lele utuh beku. Ikan lele pilihan yang diambil langsung dari peternak lele. Cocok untuk pecel lele, lele goreng, dan berbagai kreasi lainnya. Sudah dibersihkan isi perut.",
+  },
+];
